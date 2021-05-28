fe6e78f5bc475cce5da54253ef84c4ba97e07c7c
Ane Mendes <ralianemc@gmail.com>
2021-05-28 16:16:06 -0300

    Implementacao de modais para cadastro de novo curso e atividade

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,29 +1,44 @@
-import {FiLogIn, FiMail, FiLock} from 'react-icons/fi'
-import { Content, Container, FormContainer, InputContainer, Background } from "./styles";
-import { Button } from '../../components/Button';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { Content, Container, FormContainer, InputContainer, Background, Error } from "./styles";
+import { Button } from '../../components/Button';
+
+interface FormData{
+    email: string;
+    password: string;
+}
 
 export function Login (){
+
+    const {register, handleSubmit, formState: {errors}} = useForm<FormData>();
+
+    const onSubmit = handleSubmit(data => alert(JSON.stringify(data)))
+
     return (
         <Container>
             <Content>
                 <FormContainer>
                     <h2>Faça seu login</h2>
-                    <form action="">
+                    <form onSubmit={onSubmit}>
                         <InputContainer>
                         <FiMail size={40} />
                             <input
                              type="email" 
                              placeholder="email"
+                             {...register("email", {required:true})}
                              />
                         </InputContainer>
+                        {errors.email && <Error>O preenchimento do campo é obrigatório</Error>}
                         <InputContainer>
                         <FiLock size={40} />
                             <input
                              type="password" 
                              placeholder="senha"
+                             {...register("password", {required:true})}
                              />
                         </InputContainer>
+                        {errors.password && <Error>O preenchimento do campo é obrigatório</Error>}
                         <Button type="submit"> Login </Button>
                     </form>
                     <Link to="/register">
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,36 +1,53 @@
-import {FiArrowLeft, FiMail, FiLock, FiUser} from 'react-icons/fi'
-import { Content, Container, FormContainer, InputContainer, Background } from "./styles";
+import {FiArrowLeft, FiMail, FiLock, FiUser} from 'react-icons/fi';
+import { useForm } from 'react-hook-form';
+import { Content, Container, FormContainer, InputContainer, Background, Error } from "./styles";
 import { Button } from '../../components/Button';
 import { Link } from 'react-router-dom';
 
+interface FormData{
+    name: string;
+    email: string;
+    password: string;
+}
+
 export function Register (){
+    const {register, handleSubmit, formState: {errors}} = useForm<FormData>();
+
+    const onSubmit = handleSubmit(data => alert(JSON.stringify(data)))
+
     return (
         <Container>
             <Content>
                 <FormContainer>
                     <h2>Faça seu cadastro</h2>
-                    <form action="">
+                    <form onSubmit={onSubmit}>
                         <InputContainer>
                         <FiUser size={40} />
                             <input
                              type="text" 
                              placeholder="Nome"
+                             {...register("name", {required:true})}
                              />
                         </InputContainer>
+                        {errors.name && <Error>O preenchimento do campo é obrigatório</Error>}
                         <InputContainer>
                         <FiMail size={40} />
                             <input
                              type="email" 
                              placeholder="email"
+                             {...register("email", {required:true})}
                              />
                         </InputContainer>
+                        {errors.email && <Error>O preenchimento do campo é obrigatório</Error>}
                         <InputContainer>
                         <FiLock size={40} />
                             <input
                              type="password" 
                              placeholder="senha"
+                             {...register("password", {required:true})}
                              />
                         </InputContainer>
+                        {errors.password && <Error>O preenchimento do campo é obrigatório</Error>}
                         <Button type="submit"> Cadastrar </Button>
                     </form>
                     <Link to="/">
